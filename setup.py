b479a146372332eef03ce8736f8ecc4b69b5c215
Yu Cao <tsaoyu@tsaoyu.com>
2018-03-05 10:05:05 +0000

    Apply LGPL

--- setup.py
+++ setup.py
@@ -2,16 +2,18 @@
 # PyVisiLibity: a Python binding of VisiLibity1
 # Copyright (C) 2018 Yu Cao < University of Southampton> Yu.Cao at soton.ac.uk
 #
-# PyVisiLibity is free software: you can redistribute it and/or modify it under
-# the terms of the GNU Lesser General Public License as published by the
-# Free Software Foundation, either version 3 of the License, or (at your
-# option) any later version.
-# PyVisiLibity is distributed in the hope that it will be useful, but WITHOUT
-# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
-# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
-# License for more details.
-# You should have received a copy of the GNU Lesser General Public
-# License along with PyVisiLibity.  If not, see <http://www.gnu.org/licenses/>.
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 from distutils.core import setup, Extension
 from distutils.command.build import build
